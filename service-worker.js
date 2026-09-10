f3adf342acfc66632c171771b67638d8fb7cbd22
Sorasuk Thongkum <sorasukt01@gmail.com>
2026-09-10 14:42:32 +0700

    Match homepage to approved airy icon-led design

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="sorasukt-tarot-shell-v2-audit";
+const CACHE_NAME="sorasukt-tarot-shell-v3-home-reference";
 const APP_SHELL=[...new Set([
   "/tarot/",
   "/tarot/admin/admin-codes.css",
