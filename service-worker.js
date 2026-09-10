557b953b3f4378d77e1988584532c83ddf1ae687
Sorasuk Thongkum <sorasukt01@gmail.com>
2026-09-10 21:41:38 +0700

    merge main and resolve Phase 2 conflicts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="sorasukt-tarot-shell-v7-reflection-phase2";
+const CACHE_NAME="sorasukt-tarot-shell-v9-reflection-phase2";
 const APP_SHELL=[...new Set([
   "/tarot/",
   "/tarot/updates/",
