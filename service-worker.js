ff1de8172657603d16fbb037aab67de38de585af
Sorasuk Thongkum <sorasukt01@gmail.com>
2026-09-10 21:38:13 +0700

    feat: add phase 2 reflection journal

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="sorasukt-tarot-shell-v6-reading-updates";
+const CACHE_NAME="sorasukt-tarot-shell-v7-reflection-phase2";
 const APP_SHELL=[...new Set([
   "/tarot/",
   "/tarot/updates/",
